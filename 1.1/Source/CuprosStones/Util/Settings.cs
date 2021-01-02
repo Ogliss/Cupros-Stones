628b9287fbf1895d05c49bec526b7120fef57e7b
Ogliss <randomdragon2@hotmail.com>
2021-01-02 13:41:49 +0000

    added mod option to disable Cupro's Textures

--- 1.1/Source/CuprosStones/Util/Settings.cs
+++ 1.1/Source/CuprosStones/Util/Settings.cs
@@ -1,98 +1,148 @@
-﻿using Verse;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
 
-namespace CuprosStones {
+namespace CuprosStones
+{
+    public class Settings : ModSettings
+    {
+        internal static IntRange StoneTypesAvailable = new IntRange(4, 5);
 
-  public class Settings : ModSettings {
+        internal static bool SpawnLimestone = true;
+        internal static bool SpawnSandstone = true;
+        internal static bool SpawnClaystone = true;
+        internal static bool SpawnAndesite = true;
+        internal static bool SpawnSyenite = true;
 
-    internal static IntRange StoneTypesAvailable = new IntRange(4, 5);
+        internal static bool SpawnGneiss = true;
+        internal static bool SpawnMarble = true;
+        internal static bool SpawnQuartzite = true;
+        internal static bool SpawnSlate = true;
+        internal static bool SpawnSchist = true;
 
-    internal static bool SpawnLimestone = true;
-    internal static bool SpawnSandstone = true;
-    internal static bool SpawnClaystone = true;
-    internal static bool SpawnAndesite = true;
-    internal static bool SpawnSyenite = true;
-
-    internal static bool SpawnGneiss = true;
-    internal static bool SpawnMarble = true;
-    internal static bool SpawnQuartzite = true;
-    internal static bool SpawnSlate = true;
-    internal static bool SpawnSchist = true;
-
-    internal static bool SpawnGabbro = true;
-    internal static bool SpawnGranite = true;
-    internal static bool SpawnDiorite = true;
-    internal static bool SpawnDunite = true;
-    internal static bool SpawnPegmatite = true;
-
-
-		public override void ExposeData() {
-      base.ExposeData();
-      Scribe_Values.Look(ref StoneTypesAvailable, "StoneTypesAvailable", new IntRange(4, 5));
-      Scribe_Values.Look(ref SpawnLimestone, "SpawnLimestone", true);
-      Scribe_Values.Look(ref SpawnSandstone, "SpawnSandstone", true);
-      Scribe_Values.Look(ref SpawnClaystone, "SpawnClaystone", true);
-      Scribe_Values.Look(ref SpawnAndesite, "SpawnAndesite", true);
-      Scribe_Values.Look(ref SpawnSyenite, "SpawnSyenite", true);
-      Scribe_Values.Look(ref SpawnGneiss, "SpawnGneiss", true);
-      Scribe_Values.Look(ref SpawnMarble, "SpawnMarble", true);
-      Scribe_Values.Look(ref SpawnQuartzite, "SpawnQuartzite", true);
-      Scribe_Values.Look(ref SpawnSlate, "SpawnSlate", true);
-      Scribe_Values.Look(ref SpawnSchist, "SpawnSchist", true);
-      Scribe_Values.Look(ref SpawnGabbro, "SpawnGabbro", true);
-      Scribe_Values.Look(ref SpawnGranite, "SpawnGranite", true);
-      Scribe_Values.Look(ref SpawnDiorite, "SpawnDiorite", true);
-      Scribe_Values.Look(ref SpawnDunite, "SpawnDunite", true);
-      Scribe_Values.Look(ref SpawnPegmatite, "SpawnPegmatite", true);
-		}
+        internal static bool SpawnGabbro = true;
+        internal static bool SpawnGranite = true;
+        internal static bool SpawnDiorite = true;
+        internal static bool SpawnDunite = true;
+        internal static bool SpawnPegmatite = true;
 
 
-    public static bool StoneAllowedOrUndefined(ThingDef stone) {
-      if (stone == Static.Limestone) {
-        return SpawnLimestone;
-      }
-      if (stone == Static.Sandstone) {
-        return SpawnSandstone;
-      }
-      if (stone == Static.Claystone) {
-        return SpawnClaystone;
-      }
-      if (stone == Static.Andesite) {
-        return SpawnAndesite;
-      }
-      if (stone == Static.Syenite) {
-        return SpawnSyenite;
-      }
-      if (stone == Static.Gneiss) {
-        return SpawnGneiss;
-      }
-      if (stone == Static.Marble) {
-        return SpawnMarble;
-      }
-      if (stone == Static.Quartzite) {
-        return SpawnQuartzite;
-      }
-      if (stone == Static.Slate) {
-        return SpawnSlate;
-      }
-      if (stone == Static.Schist) {
-        return SpawnSchist;
-      }
-      if (stone == Static.Gabbro) {
-        return SpawnGabbro;
-      }
-      if (stone == Static.Granite) {
-        return SpawnGranite;
-      }
-      if (stone == Static.Diorite) {
-        return SpawnDiorite;
-      }
-      if (stone == Static.Dunite) {
-        return SpawnDunite;
-      }
-      if (stone == Static.Pegmatite) {
-        return SpawnPegmatite;
-      }
-      return true;
+        private Dictionary<string, bool> _scribeHelper;
+        public Dictionary<PatchDescription, bool> PatchDisabled = StonesMod.Patches.ToDictionary(p => p, p => true);
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref StoneTypesAvailable, "StoneTypesAvailable", new IntRange(4, 5));
+            Scribe_Values.Look(ref SpawnLimestone, "SpawnLimestone", true);
+            Scribe_Values.Look(ref SpawnSandstone, "SpawnSandstone", true);
+            Scribe_Values.Look(ref SpawnClaystone, "SpawnClaystone", true);
+            Scribe_Values.Look(ref SpawnAndesite, "SpawnAndesite", true);
+            Scribe_Values.Look(ref SpawnSyenite, "SpawnSyenite", true);
+            Scribe_Values.Look(ref SpawnGneiss, "SpawnGneiss", true);
+            Scribe_Values.Look(ref SpawnMarble, "SpawnMarble", true);
+            Scribe_Values.Look(ref SpawnQuartzite, "SpawnQuartzite", true);
+            Scribe_Values.Look(ref SpawnSlate, "SpawnSlate", true);
+            Scribe_Values.Look(ref SpawnSchist, "SpawnSchist", true);
+            Scribe_Values.Look(ref SpawnGabbro, "SpawnGabbro", true);
+            Scribe_Values.Look(ref SpawnGranite, "SpawnGranite", true);
+            Scribe_Values.Look(ref SpawnDiorite, "SpawnDiorite", true);
+            Scribe_Values.Look(ref SpawnDunite, "SpawnDunite", true);
+            Scribe_Values.Look(ref SpawnPegmatite, "SpawnPegmatite", true);
+            if (Scribe.mode == LoadSaveMode.Saving)
+            {
+                // create the data structure we're going to save.
+                _scribeHelper = PatchDisabled.ToDictionary(
+                    // delegate to transform a dict item into a key, we want the file property of the old key. ( PatchDescription => string )
+                    k => k.Key.file,
+
+                    // same for the value, which is just the value. ( bool => bool )
+                    v => v.Value);
+            }
+            Scribe_Collections.Look(ref _scribeHelper, "patches", LookMode.Value, LookMode.Value);
+
+            // finally, when the scribe finishes, we need to transform this back to a data structure we understand.
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                // for each stored patch, update the value in our dictionary.
+                if (!_scribeHelper.EnumerableNullOrEmpty())
+                {
+                    foreach (var storedPatch in _scribeHelper)
+                    {
+                        var index = StonesMod.Patches.FindIndex(p => p.file == storedPatch.Key);
+                        if (index >= 0) // match found
+                        {
+                            var patch = StonesMod.Patches[index];
+                            PatchDisabled[patch] = storedPatch.Value;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public static bool StoneAllowedOrUndefined(ThingDef stone)
+        {
+            if (stone == Static.Limestone)
+            {
+                return SpawnLimestone;
+            }
+            if (stone == Static.Sandstone)
+            {
+                return SpawnSandstone;
+            }
+            if (stone == Static.Claystone)
+            {
+                return SpawnClaystone;
+            }
+            if (stone == Static.Andesite)
+            {
+                return SpawnAndesite;
+            }
+            if (stone == Static.Syenite)
+            {
+                return SpawnSyenite;
+            }
+            if (stone == Static.Gneiss)
+            {
+                return SpawnGneiss;
+            }
+            if (stone == Static.Marble)
+            {
+                return SpawnMarble;
+            }
+            if (stone == Static.Quartzite)
+            {
+                return SpawnQuartzite;
+            }
+            if (stone == Static.Slate)
+            {
+                return SpawnSlate;
+            }
+            if (stone == Static.Schist)
+            {
+                return SpawnSchist;
+            }
+            if (stone == Static.Gabbro)
+            {
+                return SpawnGabbro;
+            }
+            if (stone == Static.Granite)
+            {
+                return SpawnGranite;
+            }
+            if (stone == Static.Diorite)
+            {
+                return SpawnDiorite;
+            }
+            if (stone == Static.Dunite)
+            {
+                return SpawnDunite;
+            }
+            if (stone == Static.Pegmatite)
+            {
+                return SpawnPegmatite;
+            }
+            return true;
+        }
     }
-  }
 }
--- 1.1/Source/CuprosStones/Util/StonesMod.cs
+++ 1.1/Source/CuprosStones/Util/StonesMod.cs
@@ -1,190 +1,261 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-
 using HarmonyLib;
 using UnityEngine;
 using Verse;
 
-namespace CuprosStones {
+namespace CuprosStones
+{
 
-  public sealed class StonesMod : Mod {
+    public sealed class StonesMod : Mod
+    {
+        public static Settings settings;
+        public static Harmony harmony = new Harmony("com.ogliss.rimworld.mod.CuprosStones");
+        public StonesMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<Settings>();
 
-
-		public StonesMod(ModContentPack content) : base(content) {
-      new Harmony("CuprosStonesOverride").PatchAll(Assembly.GetExecutingAssembly());
-      GetSettings<Settings>();
-		}
-
-
-    public override string SettingsCategory() {
-      return Static.LabelModName;
-    }
-
-
-    public override void DoSettingsWindowContents(Rect rect) {
-			Listing_Standard list = new Listing_Standard() {
-				ColumnWidth = rect.width
-			};
-			list.Begin(rect);
-      list.Gap(10);
-      {
-        Rect fullRect = list.GetRect(Text.LineHeight);
-        Rect leftRect = fullRect.LeftHalf().Rounded();
-        Rect rightRect = fullRect.RightHalf().Rounded();
-
-        GenUI.SetLabelAlign(TextAnchor.MiddleRight);
-        Widgets.Label(leftRect, Static.LabelStoneTypesAvailable);
-        GenUI.ResetLabelAlign();
-
-        Widgets.IntRange(rightRect, 316192000, ref Settings.StoneTypesAvailable, 1, 8);
-      }
-
-      list.Gap(25);
-      {
-				Rect fullRect = list.GetRect(30f);
-        Rect leftLabelRect = fullRect.LeftHalf().Rounded();
-				Rect rightLabelRect = fullRect.RightHalf().Rounded();
-
-        Text.Font = GameFont.Medium;
-				Text.Anchor = TextAnchor.MiddleCenter;
-
-        Widgets.Label(leftLabelRect, Static.LabelStoneTypesToSpawn);
-				
-				Text.Anchor = TextAnchor.UpperLeft;
-				Text.Font = GameFont.Small;
-			}
-
-			list.Gap(10);
-      {
-				Rect fullRect = list.GetRect(30f);
-				Rect rightLabelRect = fullRect.RightHalf().Rounded();
-
-				DualCheckboxesWithIcons_ThingDef(
-					fullRect.LeftHalf(),
-					Static.ChunkLimestone, Static.ChunkSandstone,
-					Static.Limestone?.LabelCap, Static.Sandstone?.LabelCap,
-					ref Settings.SpawnLimestone, ref Settings.SpawnSandstone
-				);
-			}
-      list.Gap(10);
-
-			{
-				Rect fullRect = list.GetRect(30f);
-				DualCheckboxesWithIcons_ThingDef(
-					fullRect.LeftHalf(),
-					Static.ChunkClaystone, Static.ChunkAndesite,
-					Static.Claystone?.LabelCap, Static.Andesite?.LabelCap,
-					ref Settings.SpawnClaystone, ref Settings.SpawnAndesite
-				);
-			}
-      list.Gap(10);
-
-			{
-				Rect fullRect = list.GetRect(30f);
-				DualCheckboxesWithIcons_ThingDef(
-					fullRect.LeftHalf(),
-					Static.ChunkSyenite, Static.ChunkGneiss,
-					Static.Syenite?.LabelCap, Static.Gneiss?.LabelCap,
-					ref Settings.SpawnSyenite, ref Settings.SpawnGneiss
-				);
-			}
-      list.Gap(10);
-
-			{
-				Rect fullRect = list.GetRect(30f);
-				DualCheckboxesWithIcons_ThingDef(
-					fullRect.LeftHalf(),
-					Static.ChunkMarble, Static.ChunkQuartzite,
-					Static.Marble?.LabelCap, Static.Quartzite?.LabelCap,
-					ref Settings.SpawnMarble, ref Settings.SpawnQuartzite
-				);
-			}
-      list.Gap(10);
-
-			{
-				Rect fullRect = list.GetRect(30f);
-				DualCheckboxesWithIcons_ThingDef(
-					fullRect.LeftHalf(),
-					Static.ChunkSlate, Static.ChunkSchist,
-					Static.Slate?.LabelCap, Static.Schist?.LabelCap,
-					ref Settings.SpawnSlate, ref Settings.SpawnSchist
-				);
-			}
-      list.Gap(10);
-
-			{
-				Rect fullRect = list.GetRect(30f);
-				DualCheckboxesWithIcons_ThingDef(
-					fullRect.LeftHalf(),
-					Static.ChunkGabbro, Static.ChunkGranite,
-					Static.Gabbro?.LabelCap, Static.Granite?.LabelCap,
-					ref Settings.SpawnGabbro, ref Settings.SpawnGranite
-				);
-      }
-      list.Gap(10);
-
-			{
-				Rect fullRect = list.GetRect(30f);
-				DualCheckboxesWithIcons_ThingDef(
-					fullRect.LeftHalf(),
-					Static.ChunkDiorite, Static.ChunkDunite,
-					Static.Diorite?.LabelCap, Static.Dunite?.LabelCap,
-					ref Settings.SpawnDiorite, ref Settings.SpawnDunite
-				);
-      }
-      list.Gap(10);
-
-			{
-				Rect fullRect = list.GetRect(30f);
-				Rect leftRect = fullRect.LeftHalf().LeftHalf().RightPartPixels(150).Rounded();
-        Rect leftIconRect = fullRect.LeftHalf().LeftHalf().LeftHalf().LeftHalf().RightPartPixels(30).Rounded();
-
-        Widgets.ThingIcon(leftIconRect, Static.ChunkPegmatite);
-
-        Widgets.CheckboxLabeled(leftRect, Static.Pegmatite?.LabelCap, ref Settings.SpawnPegmatite);
-        Widgets.DrawHighlightIfMouseover(leftRect);
-      }
-
-      GenUI.ResetLabelAlign();
-      list.End();
-    }
+            var allPatches = content.Patches as List<PatchOperation>;
+            foreach (var patch in Patches)
+            {
+                if (settings.PatchDisabled[patch] == false)
+                {
+                //    if (Prefs.DevMode) Log.Message("RemoveAll XML Patch: " + patch.label);
+                    allPatches.RemoveAll(p => p.sourceFile.EndsWith(patch.file));
+                }
+                else
+                {
+                //    if (Prefs.DevMode) Log.Message("Running XML Patch: " + patch.label);
+                }
+            }
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
 
 
-		private static void DualCheckboxesWithIcons_ThingDef(Rect rect, ThingDef leftThingDef, ThingDef rightThingDef, string leftLabel, string rightLabel, ref bool leftBool, ref bool rightBool) {
+        public override string SettingsCategory()
+        {
+            return Static.LabelModName;
+        }
+
+
+        public override void DoSettingsWindowContents(Rect rect)
+        {
+            Listing_Standard list = new Listing_Standard()
+            {
+                ColumnWidth = rect.width
+            };
+            list.Begin(rect);
+            list.Gap(10);
+            {
+                Rect fullRect = list.GetRect(Text.LineHeight);
+                Rect leftRect = fullRect.LeftHalf().Rounded();
+                Rect rightRect = fullRect.RightHalf().Rounded();
+
+                GenUI.SetLabelAlign(TextAnchor.MiddleRight);
+                Widgets.Label(leftRect, Static.LabelStoneTypesAvailable);
+                GenUI.ResetLabelAlign();
+
+                Widgets.IntRange(rightRect, 316192000, ref Settings.StoneTypesAvailable, 1, 8);
+            }
+
+            list.Gap(25);
+            {
+                Rect fullRect = list.GetRect(30f);
+                Rect leftLabelRect = fullRect.LeftHalf().Rounded();
+                Rect rightLabelRect = fullRect.RightHalf().Rounded();
+
+                Text.Font = GameFont.Medium;
+                Text.Anchor = TextAnchor.MiddleCenter;
+
+                Widgets.Label(leftLabelRect, Static.LabelStoneTypesToSpawn);
+                PatchDescription GraphicsPatch = patches.Find(x => x.file == "Patches_Core_Stone_CustomGraphics.xml");
+                var status = settings.PatchDisabled[GraphicsPatch];
+                Widgets.CheckboxLabeled(rightLabelRect, "Enable Cupro'ss Stone Textures", ref status);
+                settings.PatchDisabled[GraphicsPatch] = status;
+
+                Text.Anchor = TextAnchor.UpperLeft;
+                Text.Font = GameFont.Small;
+            }
+
+            list.Gap(10);
+            {
+                Rect fullRect = list.GetRect(30f);
+                Rect rightLabelRect = fullRect.RightHalf().Rounded();
+
+                DualCheckboxesWithIcons_ThingDef(
+                    fullRect.LeftHalf(),
+                    Static.ChunkLimestone, Static.ChunkSandstone,
+                    Static.Limestone?.LabelCap, Static.Sandstone?.LabelCap,
+                    ref Settings.SpawnLimestone, ref Settings.SpawnSandstone
+                );
+            }
+            list.Gap(10);
+
+            {
+                Rect fullRect = list.GetRect(30f);
+                DualCheckboxesWithIcons_ThingDef(
+                    fullRect.LeftHalf(),
+                    Static.ChunkClaystone, Static.ChunkAndesite,
+                    Static.Claystone?.LabelCap, Static.Andesite?.LabelCap,
+                    ref Settings.SpawnClaystone, ref Settings.SpawnAndesite
+                );
+            }
+            list.Gap(10);
+
+            {
+                Rect fullRect = list.GetRect(30f);
+                DualCheckboxesWithIcons_ThingDef(
+                    fullRect.LeftHalf(),
+                    Static.ChunkSyenite, Static.ChunkGneiss,
+                    Static.Syenite?.LabelCap, Static.Gneiss?.LabelCap,
+                    ref Settings.SpawnSyenite, ref Settings.SpawnGneiss
+                );
+            }
+            list.Gap(10);
+
+            {
+                Rect fullRect = list.GetRect(30f);
+                DualCheckboxesWithIcons_ThingDef(
+                    fullRect.LeftHalf(),
+                    Static.ChunkMarble, Static.ChunkQuartzite,
+                    Static.Marble?.LabelCap, Static.Quartzite?.LabelCap,
+                    ref Settings.SpawnMarble, ref Settings.SpawnQuartzite
+                );
+            }
+            list.Gap(10);
+
+            {
+                Rect fullRect = list.GetRect(30f);
+                DualCheckboxesWithIcons_ThingDef(
+                    fullRect.LeftHalf(),
+                    Static.ChunkSlate, Static.ChunkSchist,
+                    Static.Slate?.LabelCap, Static.Schist?.LabelCap,
+                    ref Settings.SpawnSlate, ref Settings.SpawnSchist
+                );
+            }
+            list.Gap(10);
+
+            {
+                Rect fullRect = list.GetRect(30f);
+                DualCheckboxesWithIcons_ThingDef(
+                    fullRect.LeftHalf(),
+                    Static.ChunkGabbro, Static.ChunkGranite,
+                    Static.Gabbro?.LabelCap, Static.Granite?.LabelCap,
+                    ref Settings.SpawnGabbro, ref Settings.SpawnGranite
+                );
+            }
+            list.Gap(10);
+
+            {
+                Rect fullRect = list.GetRect(30f);
+                DualCheckboxesWithIcons_ThingDef(
+                    fullRect.LeftHalf(),
+                    Static.ChunkDiorite, Static.ChunkDunite,
+                    Static.Diorite?.LabelCap, Static.Dunite?.LabelCap,
+                    ref Settings.SpawnDiorite, ref Settings.SpawnDunite
+                );
+            }
+            list.Gap(10);
+
+            {
+                Rect fullRect = list.GetRect(30f);
+                Rect leftRect = fullRect.LeftHalf().LeftHalf().RightPartPixels(150).Rounded();
+                Rect leftIconRect = fullRect.LeftHalf().LeftHalf().LeftHalf().LeftHalf().RightPartPixels(30).Rounded();
+
+                Widgets.ThingIcon(leftIconRect, Static.ChunkPegmatite);
+
+                Widgets.CheckboxLabeled(leftRect, Static.Pegmatite?.LabelCap, ref Settings.SpawnPegmatite);
+                Widgets.DrawHighlightIfMouseover(leftRect);
+            }
+
+            GenUI.ResetLabelAlign();
+            list.End();
+        }
+
+        public static void CheckboxLabeled(Rect rect, string label, ref bool checkOn, string tooltip = null, bool disabled = false, Texture2D texChechked = null, Texture2D texUnchechked = null, bool placeCheckboxNearText = false)
+        {
+            if (!tooltip.NullOrEmpty())
+            {
+                if (Mouse.IsOver(rect))
+                {
+                    Widgets.DrawHighlight(rect);
+                }
+                TooltipHandler.TipRegion(rect, tooltip);
+            }
+            Widgets.CheckboxLabeled(rect, label, ref checkOn, disabled, texChechked, texUnchechked, placeCheckboxNearText);
+            // base.Gap(this.verticalSpacing);
+        }
+
+        private static void DualCheckboxesWithIcons_ThingDef(Rect rect, ThingDef leftThingDef, ThingDef rightThingDef, string leftLabel, string rightLabel, ref bool leftBool, ref bool rightBool)
+        {
 
             if (leftThingDef != null)
-			{
-				Rect leftRect = rect.LeftHalf().RightPartPixels(150).Rounded();
-				Rect leftIconRect = rect.LeftHalf().LeftHalf().LeftHalf().RightPartPixels(30).Rounded();
-				Widgets.ThingIcon(leftIconRect, leftThingDef);
-				Widgets.CheckboxLabeled(leftRect, leftLabel, ref leftBool);
-				Widgets.DrawHighlightIfMouseover(leftRect);
-			}
+            {
+                Rect leftRect = rect.LeftHalf().RightPartPixels(150).Rounded();
+                Rect leftIconRect = rect.LeftHalf().LeftHalf().LeftHalf().RightPartPixels(30).Rounded();
+                Widgets.ThingIcon(leftIconRect, leftThingDef);
+                Widgets.CheckboxLabeled(leftRect, leftLabel, ref leftBool);
+                Widgets.DrawHighlightIfMouseover(leftRect);
+            }
             if (rightThingDef != null)
-			{
-				Rect rightRect = rect.RightHalf().RightPartPixels(150).Rounded();
-				Rect rightIconRect = rect.RightHalf().LeftHalf().LeftHalf().RightPartPixels(30).Rounded();
-				Widgets.ThingIcon(rightIconRect, rightThingDef);
-				Widgets.CheckboxLabeled(rightRect, rightLabel, ref rightBool);
-				Widgets.DrawHighlightIfMouseover(rightRect);
-			}
-		}
+            {
+                Rect rightRect = rect.RightHalf().RightPartPixels(150).Rounded();
+                Rect rightIconRect = rect.RightHalf().LeftHalf().LeftHalf().RightPartPixels(30).Rounded();
+                Widgets.ThingIcon(rightIconRect, rightThingDef);
+                Widgets.CheckboxLabeled(rightRect, rightLabel, ref rightBool);
+                Widgets.DrawHighlightIfMouseover(rightRect);
+            }
+        }
 
 
-		private static void DualCheckboxesWithIcons_Texture2D(Rect rect, Texture2D leftIcon, Texture2D rightIcon, string leftLabel, string rightLabel, ref bool leftBool, ref bool rightBool) {
-			Rect leftRect = rect.LeftHalf().RightPartPixels(150).Rounded();
-			Rect rightRect = rect.RightHalf().RightPartPixels(150).Rounded();
-			Rect leftIconRect = rect.LeftHalf().LeftHalf().LeftHalf().RightPartPixels(30).Rounded();
-			Rect rightIconRect = rect.RightHalf().LeftHalf().LeftHalf().RightPartPixels(30).Rounded();
+        private static void DualCheckboxesWithIcons_Texture2D(Rect rect, Texture2D leftIcon, Texture2D rightIcon, string leftLabel, string rightLabel, ref bool leftBool, ref bool rightBool)
+        {
+            Rect leftRect = rect.LeftHalf().RightPartPixels(150).Rounded();
+            Rect rightRect = rect.RightHalf().RightPartPixels(150).Rounded();
+            Rect leftIconRect = rect.LeftHalf().LeftHalf().LeftHalf().RightPartPixels(30).Rounded();
+            Rect rightIconRect = rect.RightHalf().LeftHalf().LeftHalf().RightPartPixels(30).Rounded();
 
-			GUI.DrawTexture(leftIconRect, leftIcon);
-			GUI.DrawTexture(rightIconRect, rightIcon);
+            GUI.DrawTexture(leftIconRect, leftIcon);
+            GUI.DrawTexture(rightIconRect, rightIcon);
 
-			Widgets.CheckboxLabeled(leftRect, leftLabel, ref leftBool);
-			Widgets.CheckboxLabeled(rightRect, rightLabel, ref rightBool);
-			Widgets.DrawHighlightIfMouseover(leftRect);
-			Widgets.DrawHighlightIfMouseover(rightRect);
-		}
-	}
+            Widgets.CheckboxLabeled(leftRect, leftLabel, ref leftBool);
+            Widgets.CheckboxLabeled(rightRect, rightLabel, ref rightBool);
+            Widgets.DrawHighlightIfMouseover(leftRect);
+            Widgets.DrawHighlightIfMouseover(rightRect);
+        }
+
+        private static List<PatchDescription> patches;
+        public static List<PatchDescription> Patches
+        {
+            get
+            {
+                if (patches.NullOrEmpty())
+                {
+                    patches = new List<PatchDescription>();
+                    patches.Add(new PatchDescription("Patches_Core_Stone_CustomGraphics.xml", "Enable Custom Stone graphics", "Enables Cupros stone graphics, if this is disabled, all stone will use vanilla graphics"));
+                    if (true)
+                    {
+
+                    }
+                }
+                return patches;
+            }
+        }
+
+    }
+    public struct PatchDescription
+    {
+        public string file;
+        public string label;
+        public string tooltip;
+
+        public PatchDescription(string file, string label, string tooltip = null)
+        {
+            this.file = file;
+            this.label = label;
+            this.tooltip = tooltip;
+        }
+    }
 }
