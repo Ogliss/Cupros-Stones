004388a06bdc311fbf8cfcfc0d748e1746919321
Ogliss <randomdragon2@hotmail.com>
2024-05-08 13:58:09 +0100

    1,5 update

---
+++ 1.5/Source/CuprosStones/Static/StoneDefOf.cs
@@ -0,0 +1,66 @@
+﻿using RimWorld;
+using Verse;
+
+namespace CuprosStones
+{
+    [DefOf]
+    public static class StoneDefOf
+    {
+        public static ThingDef Limestone;
+        public static ThingDef ChunkLimestone;
+        public static ThingDef Sandstone;
+        public static ThingDef ChunkSandstone;
+        public static ThingDef Claystone;
+        public static ThingDef ChunkClaystone;
+        public static ThingDef Andesite;
+        public static ThingDef ChunkAndesite;
+        public static ThingDef Syenite;
+        public static ThingDef ChunkSyenite;
+
+        public static ThingDef Gneiss;
+        public static ThingDef ChunkGneiss;
+        public static ThingDef Marble;
+        public static ThingDef ChunkMarble;
+        public static ThingDef Quartzite;
+        public static ThingDef ChunkQuartzite;
+        public static ThingDef Slate;
+        public static ThingDef ChunkSlate;
+        public static ThingDef Schist;
+        public static ThingDef ChunkSchist;
+
+        public static ThingDef Gabbro;
+        public static ThingDef ChunkGabbro;
+        public static ThingDef Granite;
+        public static ThingDef ChunkGranite;
+        public static ThingDef Diorite;
+        public static ThingDef ChunkDiorite;
+        public static ThingDef Dunite;
+        public static ThingDef ChunkDunite;
+        public static ThingDef Pegmatite;
+        public static ThingDef ChunkPegmatite;
+
+        //// TWC Additions - Have to create own thingDefs for
+        //// New Smooth Job introduced in B19
+        //public static ThingDef TWC_SmoothedSandstone;
+        //public static ThingDef TWC_SmoothedGranite;
+        //public static ThingDef TWC_SmoothedLimestone;
+        //public static ThingDef TWC_SmoothedSlate;
+        //public static ThingDef TWC_SmoothedMarble;
+
+        public static ThingDef SmoothedClaystone;
+        public static ThingDef SmoothedAndesite;
+        public static ThingDef SmoothedSyenite;
+
+        public static ThingDef SmoothedGneiss;
+        public static ThingDef SmoothedMarble;
+        public static ThingDef SmoothedQuartzite;
+        public static ThingDef SmoothedSlate;
+        public static ThingDef SmoothedSchist;
+
+        public static ThingDef SmoothedGabbro;
+        public static ThingDef SmoothedGranite;
+        public static ThingDef SmoothedDiorite;
+        public static ThingDef SmoothedDunite;
+        public static ThingDef SmoothedPegmatite;
+    }
+}
